94c59deec41fa8cfbf4880a466d3ab08aae5d77d
Mattias Besemann <mattias.besemann@gmail.com>
2020-08-26 21:27:52 -0400

    Add category filtering

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -35,6 +35,15 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/products/:category", (req, res) => {
+    db.Product.findAll({
+      raw: true,
+      include: { model: db.Category, where: { name: req.params.category } },
+    }).then(products => {
+      res.render("products", { products: products });
+    });
+  });
+
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
